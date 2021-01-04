1e0f61e98b4376a0b266d7d5ad5e1edfe888e143
Francisco Antonio Cabezas Vega <francisco.cabezas@zabud.com.co>
2021-01-03 21:22:27 -0500

    Step 2.1

---
+++ 2_Cursos_Selenium/selenium-framework-poc/src/test/java/com/mesaj/app/pageobjects/SignUpPageObject.java
@@ -0,0 +1,9 @@
+package com.mesaj.app.pageobjects;
+
+import org.openqa.selenium.By;
+
+public class SignUpPageObject {
+
+    private By firstNameTextbox = By.xpath("//input[@ng-model='FirstName']");
+
+}
