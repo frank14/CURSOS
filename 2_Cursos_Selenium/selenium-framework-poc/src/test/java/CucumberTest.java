4f9c66bbd4c773ba27b8d5ec8bca2d70c017ca00
Francisco Antonio Cabezas Vega <francisco.cabezas@zabud.com.co>
2021-01-11 19:16:03 -0500

    Screenshot Conditionals

--- 2_Cursos_Selenium/selenium-framework-poc/src/test/java/CucumberTest.java
+++ 2_Cursos_Selenium/selenium-framework-poc/src/test/java/CucumberTest.java
@@ -3,10 +3,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",
-                   plugin = {"de.monochromata.cucumber.report.PrettyReports:target/cucumber",
-                           "pretty", "json:target/cucumber-reports/Cucumber,json"
-                   })
+@CucumberOptions(strict = true,
+                    features = "src/test/resources/features",
+                    plugin = {"de.monochromata.cucumber.report.PrettyReports:target/cucumber",
+                       "pretty", "json:target/cucumber-reports/Cucumber,json"
+                    })
 public class CucumberTest {
 }
 
--- 2_Cursos_Selenium/selenium-framework-poc/src/test/java/com/mesaj/app/util/reporting/PhotographerHook.java
+++ 2_Cursos_Selenium/selenium-framework-poc/src/test/java/com/mesaj/app/util/reporting/PhotographerHook.java
@@ -1,27 +1,48 @@
 package com.mesaj.app.util.reporting;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.UUID;
 
 public class PhotographerHook {
+
     @Autowired
     private WebDriver webDriver;
 
+    @Value("${take.screenshot.condition:none}")
+    private TakeScreenshotCondition takeScreenshotCondition;
+
     @After
+    public void takeScreenShotAfterScenario(Scenario scenario) {
+        if (TakeScreenshotCondition.endOfScenario == takeScreenshotCondition)
+            takeScreenShot(scenario);
+    }
+
+    @AfterStep
+    public void takeScreenShotAfterEveryStep(Scenario scenario) {
+        if (TakeScreenshotCondition.everyStep == takeScreenshotCondition)
+            takeScreenShot(scenario);
+    }
+
+    @After
+    public void takeScreenShotAfterFailingScenario(Scenario scenario) {
+        if (scenario.isFailed() && TakeScreenshotCondition.failingScenario == takeScreenshotCondition)
+            takeScreenShot(scenario);
+    }
+
     public void takeScreenShot(Scenario scenario) {
 
         try {
-            if (scenario.isFailed()) {
-                final byte[] screenshot = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.BYTES);
-                scenario.embed(screenshot,"image/png", UUID.randomUUID().toString());
-            }
+            final byte[] screenshot = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot,"image/png", UUID.randomUUID().toString());
         } catch (WebDriverException wde) {
             System.out.println("There was an error taking the screenshot: " + wde.getMessage());
         }
